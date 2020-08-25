fef756c2a9b09d54a7e13949e636e1fe0a268f88
YingjunChen <425547874@qq.com>
2020-08-26 00:21:32 +0800

    Update PaymentMain8006.java

--- cloud-providerconsul-payment-8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
+++ cloud-providerconsul-payment-8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class PaymentMain8006 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8006.class,args);
+        //pull test
     }
 }
